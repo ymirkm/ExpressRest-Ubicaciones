8b76c0a9a680b4586538f12c0431b81db5f63c3d
Ymir <ymirkm@gmail.com>
2015-03-13 11:34:53 -0500

    Correcion enviando get data en expressserver

--- app/expressServer.js
+++ app/expressServer.js
@@ -14,7 +14,7 @@ var ExpressServer = function(config){
 	}
 
 	/************************************************************************** Conexión base de datos ****/
-	mongoose.connect('mongodb:localhost/ubicaciones', function(err, res){
+	mongoose.connect('mongodb://localhost/ubicaciones', function(err, res){
 		if(err){
 			console.log('Error estableciendo conexion con la base de datos ' + err);
 		}
@@ -23,30 +23,41 @@ var ExpressServer = function(config){
 	this.expressServer.get('/Hola/', function(req, res, next){
 		res.send('Hola desde Servidor Modularizado');
 	});
-
-	this.expressServer.get('/ubicaciones/', function(req, ubicaciones, next){
-		Ubicacion.find(function(err,res, next){
+	/************************************************************************** Obtener todas las ubicaciones guardadas ****/
+	this.expressServer.get('/ubicaciones/', function(req, res, next){
+		Ubicacion.find(function(err,ubicaciones){
 			if(!err){
 				res.send(ubicaciones);
 			}else{ res.send('Error en GET ' + err); }
 		});
 	});
-
+	/************************************************************************** Guardar ubicacion enviada por URL(get) ****/
 	this.expressServer.get('/postPrueba/:nombre', function(req, res){
-		console.log(req.params.nombre);
-		res.send('Peticion a /postPrueba/' + req.params.nombre);
-	});
-
-	this.expressServer.post('/postPrueba/', function(req, res){
-		console.log(req.body.nombre);
+		var paramNombre = req.params.nombre;
+		console.log(paramNombre);
 		var objSave = new Ubicacion({
-			nombre: req.body.mombre
+			nombre: paramNombre
 		});
 		objSave.save(function(err){
 			if(!err){
 				console.log('Ubicacion guardada con exito');
 			}else { console.log('Error al guardar nueva ubicacion ' + err); }
 		});
+		res.send(objSave);
+	});
+
+	this.expressServer.post('/postPrueba/', function(req, res){
+		var paramNombre = req.body.nombre;
+		console.log('pepe');
+		var objSave = new Ubicacion({
+			nombre: 'Pepe'
+		});
+		objSave.save(function(err){
+			if(!err){
+				console.log('Ubicacion guardada con exito');
+			}else { console.log('Error al guardar nueva ubicacion ' + err); }
+		});
+		res.send(objSave);
 	});
 };
 module.exports = ExpressServer;

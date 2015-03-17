0ab6ab3b67293f05b30f5ea68419a4ffaf22f6c8
Ymir <ymirkm@gmail.com>
2015-03-16 19:56:24 -0500

    Peticion Post agregada

--- app/expressServer.js
+++ app/expressServer.js
@@ -7,7 +7,6 @@ var ExpressServer = function(config){
 	config = config || {};
 
 	this.expressServer = express();
-
 	/************************************************************************** Carga de Middlewares ****/
 	for(var middleware in middlewares){
 		this.expressServer.use(middlewares[middleware]);
@@ -20,8 +19,8 @@ var ExpressServer = function(config){
 		}
 	});
 
-	this.expressServer.get('/Hola/', function(req, res, next){
-		res.send('Hola desde Servidor Modularizado');
+	this.expressServer.get('/saluda/', function(err, res){
+		res.send('Servidor saludando desde /saluda/ por metodo GET');
 	});
 	/************************************************************************** Obtener todas las ubicaciones guardadas ****/
 	this.expressServer.get('/ubicaciones/', function(req, res, next){
@@ -32,11 +31,10 @@ var ExpressServer = function(config){
 		});
 	});
 	/************************************************************************** Guardar ubicacion enviada por URL(get) ****/
-	this.expressServer.get('/postPrueba/:nombre', function(req, res){
-		var paramNombre = req.params.nombre;
-		console.log(paramNombre);
+	this.expressServer.get('/add_ubicacion/:ubicacion', function(req, res){
+		var paramNUbicaion = req.params.ubicacion;
 		var objSave = new Ubicacion({
-			nombre: paramNombre
+			nombre: paramNUbicaion
 		});
 		objSave.save(function(err){
 			if(!err){
@@ -46,9 +44,18 @@ var ExpressServer = function(config){
 		res.send(objSave);
 	});
 
-	this.expressServer.post('/postPrueba/', function(req, res){
-		var paramNombre = req.body.nombre;
-		console.log('pepe');
+	this.expressServer.get(/add_ubicacion/, function(req, res){
+		var html = '<form action="/add_ubicacion/" method="post">' +
+               'Enter your name:' +
+               '<input type="text" name="ubicacion" />' +
+               '<br>' +
+               '<button type="submit">Submit</button>' +
+            '</form>';
+		res.send(html);
+	});
+
+	this.expressServer.post('/add_ubicacion/', function(req, res){
+		/*console.log('pepe');
 		var objSave = new Ubicacion({
 			nombre: 'Pepe'
 		});
@@ -57,7 +64,9 @@ var ExpressServer = function(config){
 				console.log('Ubicacion guardada con exito');
 			}else { console.log('Error al guardar nueva ubicacion ' + err); }
 		});
-		res.send(objSave);
+		res.send(objSave);*/
+		console.log(req.body);
+		res.send('Nombre: ' + req.body.ubicacion);
 	});
 };
 module.exports = ExpressServer;
